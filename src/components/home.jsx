4a573a2311143eea98cdf6785c574096f1a9dce9
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-10-12 12:10:15 -0700

    file setup

---
+++ src/components/home.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+function Home(){
+    return (
+        <div className="home">
+            <div class="container">
+                <div class="row align-items-center my-5">
+                    <div class="col-lg-7">
+                        <img class="img-fluid rounded mb-4 mb-lg-0"
+                        src="http://placehold.it/900x400" 
+                        alt="placeholder graphic"></img>
+                    </div>
+                    <div class="col-lg-5">
+                        <h1 class="font-weight-light">Home</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint cum maiores nobis voluptas quo modi sunt recusandae fugit, eum similique atque fugiat expedita veritatis sequi voluptates? Tempora, amet. Doloremque, facilis necessitatibus fuga eligendi hic laborum dicta magnam modi eos libero!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
