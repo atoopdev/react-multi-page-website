1838ab992460d7b553ae54f27a20cc462adb11ef
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-10-13 09:57:19 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import * as serviceWorker from "./serviceWorker"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import {Navigation, Footer, Home, About, Contact, Blog, Posts, Post,} from "./components"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <Navigation/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={About />} />
+      <Route path="/contact" element={Contact />} />
+      <Route path="/blog" element={Blog />} />
+        <Route path="" element={Posts />} />
+        <Route path=":postSlug" element={<Post />} />
+    </Routes>
+  </Router>,
+
+  document.getElementById("root")
 );
+
+serviceWorker.unregister();
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
